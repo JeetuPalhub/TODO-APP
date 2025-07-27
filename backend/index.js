a7174c8d011d33715d9dad2bfa6596eb23f772e2
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-07-27 23:03:40 +0530

    .env file

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,19 @@
 import express from  'express'
+import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 const app = express()
 
 dotenv.config();
 
 const PORT =process.env.PORT || 4001
+
+
+//Database connection code
+try {
+  
+} catch (error) {
+  
+}
 
 
 app.listen(PORT, () => {
