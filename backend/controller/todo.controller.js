6bb4a0d94d3e7f2f599186dda4e153da35d7c5d4
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-07-29 20:42:19 +0530

    commit

---
+++ backend/controller/todo.controller.js
@@ -0,0 +1,5 @@
+import Todo from "../model/todo.model.js"
+
+const createTodo = () => {
+    
+}
--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,14 @@ const app = express()
 dotenv.config();
 
 const PORT =process.env.PORT || 4001
-
+const DB_URI=process.env.MONGODB_URI
 
 //Database connection code
 try {
-  
+   mongoose.connect(DB_URI)
+   console.log("Connected to MongoDB")
 } catch (error) {
-  
+  console.log(error)
 }
 
 
