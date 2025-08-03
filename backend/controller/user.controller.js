f34014602fb8855e88a595f7c93c054575e0e4da
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-08-03 21:52:48 +0530

    add file

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,5 +1,23 @@
-export const register=(req,res)=>{
-    console.log("Signup function called")
+import User from "../model/user.model.js"
+
+export const register=async(req,res)=>{
+    try {
+        const { email, username, password } =req.body
+        // console.log(email, username, password) 
+
+        const user=await User.findOne({email})
+        if(user){
+            return res.status(400).json({message: "User already registered"})
+        }
+        const newUser = new User({ email, username, password})
+        await newUser.save()
+        if(newUser){
+            res.status(201).json({message: "USer registered successfully", newUser})
+        }
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({message: "Error registering user"})
+    }
 }
 
 export const login=(req,res)=>{
