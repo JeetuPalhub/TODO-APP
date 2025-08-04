daed894c599529d80ebdbeee9c9c9e2eb443c9b4
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-08-04 17:10:51 +0530

    bcrypt added

--- backend/controller/user.controller.js
+++ backend/controller/user.controller.js
@@ -1,9 +1,25 @@
 import User from "../model/user.model.js"
+import {email, z} from"zod"
+const userSchema=z.object({
+    email:z.string().email({message:"Invalid email address"}),
+    username:z.string().min(3,{message:"Username atleast 3 character long"}),
+    password:z.string().min(6,{message:"password atleast 6 character long"})  
+})
 
 export const register=async(req,res)=>{
     try {
         const { email, username, password } =req.body
         // console.log(email, username, password) 
+
+        if(!email || !username || !password){
+            return res.status(400).json({message: "All fields are required"})
+        }
+        const validation= userSchema.safeParse({email, username, password})
+        if(!validation.success){
+            // return res.status(400).json({errors: validation.error.errors})
+            const errorMessage=validation.error.errors.map((err)=>err.message)
+            return res.status(400).json({errors:errorMessage})
+        }
 
         const user=await User.findOne({email})
         if(user){
