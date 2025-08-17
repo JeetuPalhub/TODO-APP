61ec7cb01cbd4776678fae7810cb6adbb3699272
JeetuPalhub <jeetupal.pal31@gmail.com>
2025-08-17 19:11:27 +0530

    token

--- backend/jwt/token.js
+++ backend/jwt/token.js
@@ -1,4 +1,4 @@
 // Make a token and store it in cookies
-export const generateTokenAndsaveInCookies = (req, res) => {
+export const generateTokenAndsaveInCookies = (userID, res) => {
     console.log("Token generated: ") // Right now, it just says token made, but doesn't really make one yet
 }
